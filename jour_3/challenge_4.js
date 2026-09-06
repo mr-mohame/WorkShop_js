031650043dc3e1c0c454a4f0b011881cbf2ed9c8
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 16:39:21 +0100

    feat: termine le challenge 4 pour le jour 3

---
+++ jour_3/challenge_4.js
@@ -0,0 +1,8 @@
+//Générez et affichez la table de multiplication du nombre 5 (de 5 × 1 à 5 × 10).
+let resultat;
+
+for(let i=0; i<10; i++){
+    resultat = 5 * i;
+    console.log(`5 * ${i}= ${resultat}`);
+
+}
