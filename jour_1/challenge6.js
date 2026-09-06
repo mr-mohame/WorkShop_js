fd2a3a117ef0c10b33755b1d1b670298246a74bd
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 12:04:11 +0100

    feat: termine le challenge 6

---
+++ jour_1/challenge6.js
@@ -0,0 +1,3 @@
+//Créez une variable booléenne isStudent et affichez son type exact dans la console en utilisant l'opérateu typeof .
+var isStudent = true;
+console.log("le type de variable est :",typeof isStudent);
