1131307ef10568b315c6c6818ee0547809a90f33
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 11:08:50 +0100

    feat: termine le challenge 2

---
+++ jour_1/challenge2.js
@@ -0,0 +1,4 @@
+//Créez deux variables numériques et affichez l'addition, la soustraction, la multiplication et la division de ces deux nombres.
+var a = 6;
+var b = 2;
+console.log(a+b, a-b, a*b, a/b);
