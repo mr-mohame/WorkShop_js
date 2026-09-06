4697de1769be134db679ba8bcfbb8079971c5eee
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 16:35:39 +0100

    feaat:termine le challenge 3 pour le jour 3

---
+++ jour_3/challenge_3.js
@@ -0,0 +1,6 @@
+//Calculez la somme totale de tous les nombres de 1 à 100 et affichez le résultat final.
+let somme=0
+for(let i =1; i<=100; i++){
+    somme = somme + i;
+}
+console.log(`la somme total est : ${somme}`);
