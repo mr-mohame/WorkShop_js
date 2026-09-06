8017a0254ddc6616e474e92220798716f50dd2dc
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 12:53:11 +0100

    feat: termine le challenge 9

---
+++ jour_1/challenge9.js
@@ -0,0 +1,14 @@
+//Convertissez la chaîne "45" en nombre entier, ajoutez 5 , puis convertissez le nombre 100 en chaîne de caractères.
+let text = "45";
+let nombre = Number(text);
+console.log(typeof text);
+console.log(typeof nombre);
+// ajouter 5 
+let sum = nombre + 5;
+console.log("sum = ",sum);
+
+//convert le nomber --> chaine
+let num = 100;
+let Text = String(num);
+console.log(typeof num);
+console.log(typeof Text);
