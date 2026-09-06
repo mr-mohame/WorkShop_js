a0ec2d5283f2219ea1a5235143f850a0da43dfd8
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 11:23:41 +0100

    feat termine le challenge 4

---
+++ jour_1/challenge4.js
@@ -0,0 +1,5 @@
+//Convertissez une température de Celsius en Fahrenheit avec la formule : F = C * 1.8 + 32 .
+var température = 25;
+
+var Fahrenheit = température * 1.8 + 32;
+console.log("la resultat final en fahrenheit est :", Fahrenheit);
