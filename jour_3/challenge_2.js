a074ed37bf40cd8aac0f7fc614ce3f6890bb7cb7
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 16:27:06 +0100

    feat: termine le challenge 2 pour le jour 3

---
+++ jour_3/challenge_2.js
@@ -0,0 +1,8 @@
+//Affichez uniquement les nombres pairs de 1 à 20 en utilisant une boucle while .
+const prompt = require('prompt-sync')();
+let compteur = 2;
+while(compteur <= 20){
+    console.log(`nomber ${compteur}`);
+    compteur = compteur + 2;
+   
+}
