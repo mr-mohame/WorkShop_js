3711dbd0c4d06a22deec23ab44a8c7c2decc70d3
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 11:39:48 +0100

    feat: termine le challenge 5

---
+++ jour_1/challenge5.js
@@ -0,0 +1,4 @@
+//Combinez deux chaînes ( firstName et lastName ) en utilisant les Template Literals (backticks).
+let firstName = "Mohamed";
+let lastName = "lamssiah";
+console.log(`je suis ${firstName} ${lastName}`);
