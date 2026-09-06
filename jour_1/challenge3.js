7457ee8c3d73218b93fcbf892da650166d0b4862
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 11:15:59 +0100

    feat:termine le challenge 3

---
+++ jour_1/challenge3.js
@@ -0,0 +1,6 @@
+//Calculez l'aire d'un rectangle en définissant deux variables width et height .
+var width = 12;
+var height = 5;
+
+var aire = width * height;
+console.log("le resultat final est :",aire);
