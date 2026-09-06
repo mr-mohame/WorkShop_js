eaa2c2e7cc6facb83453e640c62325d7ba60678f
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 16:09:37 +0100

    fest:termine le challenge 1 pour le jour 3

---
+++ jour_3/challenge_1.js
@@ -0,0 +1,7 @@
+//Affichez les nombres de 1 à 10 dans la console à l'aide d'une boucle for .
+const prompt = require('prompt-sync')();
+
+for(let i = 1; i <= 10; i++){
+    console.log(`nomber ${i}`);
+
+}
