23cd801f848b3e8494e5e4762af079259a66b219
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 12:32:22 +0100

    feat: termine le challenge 8

---
+++ jour_1/challenge8.js
@@ -0,0 +1,8 @@
+//Calculez le reste de la division d'un nombre par 2 avec l'opérateur % pour vérifier s'il est pair ou impair.
+
+let number = 24;
+if(number % 2 == 0){
+    console.log(`le number ${number} est pair`);
+}else{
+    console.log(`le number ${number} est impair`);
+}
