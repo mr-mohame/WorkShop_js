41aede449da2926cdde789d2ac0d75cf172d26f8
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-06 12:41:02 +0100

    feat: corrige le code

--- jour_1/challenge8.js
+++ jour_1/challenge8.js
@@ -1,4 +1,8 @@
 //Calculez le reste de la division d'un nombre par 2 avec l'opérateur % pour vérifier s'il est pair ou impair.
+let saisir = require('prompt-sync')();
+
+let a = Number(saisir("enter la valeur de a : "));
+
 
 let number = 24;
 if(number % 2 == 0){
